dde69afa0a1295667f7538a0abcbe1d76c01ad0f
Jan Gabrielsson <jan@gabrielsson.com>
2025-02-18 14:13:18 +0100

    Remove password from configuration file for improved security

--- hc3emu_cfg.lua
+++ hc3emu_cfg.lua
@@ -2,8 +2,7 @@ return {                   -- Example of configuration file
   debug = { myflag = 88 }, -- debug flags, can be overriden with --%%debug directives in file
   creds = {
     user = "admin",             -- creds for accessing the HC3
-    url = "http://192.168.1.57/",
-    password = "Admin1477!"
+    url = "http://192.168.1.57/"
   },
   secret="hysch"           -- Ex. --%%var=foo:config.secret, creates quickVar foo with value "hysch"
 }

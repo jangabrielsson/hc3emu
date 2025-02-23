eb0e9e35d37e126b13f7af18f119fc91292820a9
Jan Gabrielsson <jan@gabrielsson.com>
2025-02-23 19:34:56 +0100

    Refactor VSCode Lua script to implement download and upload commands for QA files

--- tools/vscode.lua
+++ tools/vscode.lua
@@ -1,4 +1,4 @@
-#!/usr/bin/env lua
+--#!/usr/bin/env lua
 
 if require and not QuickApp then require("hc3emu") end
 --%%color=false
@@ -6,32 +6,68 @@ if require and not QuickApp then require("hc3emu") end
 --%%silent=true
 --%%debug=info:false
 
+local function printf(fmt,...) _print(string.format(fmt,...)) end
+
+local function readFile(fn)
+  local f = io.open(fn,"r")
+  if not f then return nil end
+  local s = f:read("*a")
+  f:close()
+  return s
+end
+
 local cmd = args[1]
 local arg = args[2]
 
-print(cmd,arg)
-local function main()
-  if cmd == "updateFile" then
-    local f = io.open(".project","r")
-    if f then 
-      local p = f:read("*a")
-      f:close()
-      p = json.decode(p)
-      for qn,fn in pairs(p.files or {}) do
-        if arg==fn then 
-          _print("Updating file",p.id,fn,qn)
-          os.exit()
+local cmd = {}
+
+function cmd.downloadQA()
+  printf("Downloading QA:%s",arg) --id
+  --TBD
+end
+
+function cmd.uploadQA()
+  printf("Downloading QA:%s",arg) -- name
+  --TBD
+end
+
+function cmd.uploadFile()
+  local f = io.open(".project","r")
+  if f then 
+    local p = f:read("*a")
+    f:close()
+    p = json.decode(p)
+    for qn,fn in pairs(p.files or {}) do
+      if arg==fn then 
+        local content = readFile(fn)
+        local f  = {name=fn,content=content}
+        local r,err = api.put("/quickApps/"..p.id.."/files/"..qn,f)
+        if not r then 
+          local r,err = api.post("/quickApps/"..p.id.."/files",f)
+          if err then
+            printf("Error  QA:%s, file:%s, QAfile%s",p.id,fn,qn)
+          else
+            printf("Created QA:%s, file:%s, QAfile%s",p.id,fn,qn)
+          end
+        else 
+          printf("Updated QA:%s, file%s, QAfile:%s ",p.id,fn,qn)
         end
-        print(fn," not found in current project")
+        os.exit()
       end
-    else
-      _print("No .project file found")
+      print(fn," not found in current project")
     end
+  else
+    _print("No .project file found")
   end
 end
 
-local stat,err = pcall(main)
-if not stat then
-  _print("Error:",err)
+local c = cmd[cmd]
+if not c then
+  _print("Unknown command:",cmd)
+else
+  local stat,err = pcall(c)
+  if not stat then
+    _print("Error:",err)
+  end
 end
 os.exit()

8d87fab0a00a969aeddaa5862e4a6ca8427776a2
Jan Gabrielsson <jan@gabrielsson.com>
2025-05-02 06:34:58 +0200

    Remove QA_nodebug.lua and add QA_termtest.lua with terminal handling and key processing

---
+++ examples/QA_termtest.lua
@@ -0,0 +1,31 @@
+_DEVELOP=true
+if require and not QuickApp then require('hc3emu') end
+
+--%%name=MyQA
+--%%type=com.fibaro.binarySwitch
+--%%plugin=$hc3emu.terminal
+
+function QuickApp:onInit()
+  print("Hello")
+  setInterval(function() print("Ping") end,3000)
+  fibaro.hc3emu.terminal.setExitKey(27)
+  fibaro.hc3emu.terminal.setKeyHandler(function(key, keytype)
+    if keytype == "char" then
+      -- just a key
+      local b = key:byte()
+      if b < 32 then
+        key = "." -- replace control characters with a simple "." to not mess up the screen
+      end
+
+      print("Key received: " .. key .. " (bytes: " .. b .. ")")
+      
+    elseif keytype == "ansi" then
+      -- we got an ANSI sequence
+      local seq = { key:byte(1, #key) }
+      print("ANSI sequence received: " .. key:sub(2,-1), "(bytes: " .. table.concat(seq, ", ")..")")
+      
+    else
+      print("unknown key type received: " .. tostring(keytype))
+    end
+  end)
+end
